89d7045a6a95ca2b82e94cf357f4b974fa007bdd
SonataGMX <sonata.velevko@geomatrix.lt>
2024-11-26 12:37:48 +0200

    Update main.js layout width test

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ export default createEodash({
         id: Symbol(),
         type: "internal",
         title: "Indicators",
-        layout: { x: 0, y: 0, w: 3, h: 5 },
+        layout: { x: 0, y: 0, w: 2.5, h: 5 },
         widget: {
           name: "EodashItemFilter",
           properties: {
@@ -54,7 +54,7 @@ export default createEodash({
         id: Symbol(),
         type: "internal",
         title: "Layer Control",
-        layout: { x: 0, y: 5, w: 3, h: 7 },
+        layout: { x: 0, y: 5, w: 2.5, h: 7 },
         widget: {
           name: "EodashLayerControl",
         },
