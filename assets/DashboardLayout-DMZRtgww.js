4f1e5a53ba7ef56fc1413361241b29f28a005b1e
SonataGMX <sonata.velevko@geomatrix.lt>
2024-12-02 12:48:46 +0000

    Deploying to gh-pages from @ RamintaGMX/eodash-instance-test@5811f18ba46c2d5b1e43ade56361189db910cffe 🚀

--- assets/DashboardLayout-DMZRtgww.js
+++ assets/DashboardLayout-DMZRtgww.js
@@ -1,4 +1,4 @@
-import{_ as m,x as y,W as x,X as d,$ as t,a0 as r,a1 as l,a9 as _,a3 as n,a6 as u,a2 as c,a4 as p,d as h,a5 as g,F as f,a7 as w,ab as v}from"./index-BlP6wHXQ.js";import{V as k}from"./VMain-Bzz2D3S2.js";import"./ssrBoot-EFVIVWQD.js";class A extends HTMLElement{static get observedAttributes(){return["gap"]}constructor(){super(),this.attachShadow({mode:"open"}),this.render()}render(){this.shadowRoot.innerHTML=`
+import{_ as m,x as y,W as x,X as d,$ as t,a0 as r,a1 as l,a9 as _,a3 as n,a6 as u,a2 as c,a4 as p,d as h,a5 as g,F as f,a7 as w,ab as v}from"./index-QplpET-K.js";import{V as k}from"./VMain-8BCQnm-a.js";import"./ssrBoot-BfaY5erC.js";class A extends HTMLElement{static get observedAttributes(){return["gap"]}constructor(){super(),this.attachShadow({mode:"open"}),this.render()}render(){this.shadowRoot.innerHTML=`
     <style>
       :host {
         display: grid;
